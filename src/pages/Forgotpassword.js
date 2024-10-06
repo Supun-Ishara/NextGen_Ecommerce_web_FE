65cb5e27b773e44500677147f78dd3f437e9ac75
Supun Jayathilaka <147173476+Supun-Ishara@users.noreply.github.com>
2024-10-06 09:44:17 +0530

    Remove the redundant words

--- src/pages/Forgotpassword.js
+++ src/pages/Forgotpassword.js
@@ -2,7 +2,6 @@ import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import { Link } from 'react-router-dom';
-import Contact from "./Contact";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
 
